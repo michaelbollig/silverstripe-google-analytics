2f847711a54c7d43b654223250f6f8e007287cf8
Ralph Slooten <axllent@gmail.com>
2013-02-07 22:18:21 +1300

    Update GA code

--- code/GaTracker.php
+++ code/GaTracker.php
@@ -26,11 +26,10 @@ class GaTracker extends SiteTreeExtension {
 				$track = '_gaq.push(["_setAccount","' . GaTrackingCode . '"],["_trackPageview"]);';
 			$gacode = 'var _gaq=_gaq||[];
 				' . $track . '
-				(function(){
-					var a=document.createElement("script");
-					a.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";
-					a.setAttribute("async","true");
-					document.documentElement.firstChild.appendChild(a);
+				(function() {
+					var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
+					ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+					var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);
 				})();
 				function gaDlTracker(){
 					var a = document.getElementsByTagName("a");
