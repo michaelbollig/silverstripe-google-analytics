8464c49585c8ad953f2b4f4677751540c1eb5cdb
Ralph Slooten <axllent@gmail.com>
2013-02-12 12:59:52 -0800

    update

--- _config.php
+++ _config.php
@@ -3,5 +3,5 @@ Object::add_extension('SiteTree', 'GaTracker');
 /*
  * In order to use Google Analytics you first need to:
  * define('GaTrackingCode', 'UA-xxxxxx'); in your config
- * then add $GoogleAnalytics to your template(s)
+ * then add $GoogleAnalytics or $GoogleAnalyticsInline to your template(s)
  */
