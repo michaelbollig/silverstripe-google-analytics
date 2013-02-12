7f99c6b00346f1cf8e84726a1f4b0063c4d768e2
axllent <axllent@gmail.com>
2013-02-13 09:17:58 +1300

    Add optional external JavaScript

---
+++ javascript/gatracker.js
@@ -0,0 +1,33 @@
+var _gaq = _gaq||[];
+
+function gaLt(e) {
+	var el = e ? e.target : window.event.srcElement; /* window.event.srcElement for IE */
+	var type = el.tagName.toLowerCase();
+	if(type == 'a') {
+		var ref = document.location.pathname + document.location.search;
+		if( el.href.indexOf(location.host) == -1 ) {
+			_gaq.push(["_trackEvent","Outgoing Links",el.href,ref]);
+			if(!el.target){
+				setTimeout(function(){document.location.href=el.href;}.bind(el),500);
+				return false;
+			}
+		}
+		else if ( el.href.match(/\/assets\//) ) {
+			_gaq.push(["_trackEvent","Downloads",el.href.match(/\/assets\/(.*)/)[1],ref]);
+			if ( !el.target ) {
+				setTimeout(function(){document.location.href=el.href;}.bind(el),500);
+				return false;
+			}
+		}
+	}
+}
+
+var b = document.body;
+var o = b.onclick;
+if ( typeof o != "function" ) o = gaLt;
+else {
+	b.onclick = function() {
+		o();
+		gaLt();
+	}
+}
