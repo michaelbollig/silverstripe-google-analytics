8bad089f60f39966eaefde5d6c3db3ada6736d89
Ralph Slooten <axllent@gmail.com>
2013-02-24 18:21:02 -0800

    Use addEventListener instead

--- javascript/gatracker.js
+++ javascript/gatracker.js
@@ -1,33 +1,24 @@
-var _gaq = _gaq||[];
-
-function gaLt(e) {
-	var el = e ? e.target : window.event.srcElement; /* window.event.srcElement for IE */
-	var type = el.tagName.toLowerCase();
-	if(type == 'a') {
+var _gaq=_gaq||[];
+function _gaLt(event){
+	var el = event.srcElement || event.target;
+	if(el.tagName.toLowerCase() == 'a' && el.href){
 		var ref = document.location.pathname + document.location.search;
-		if( el.href.indexOf(location.host) == -1 ) {
+		if(el.href.indexOf(location.host) == -1){
 			_gaq.push(["_trackEvent","Outgoing Links",el.href,ref]);
 			if( !el.target ){
 				setTimeout(function(){document.location.href=el.href;}.bind(el),500);
 				return false;
 			}
 		}
-		else if ( el.href.match(/\/assets\//) ) {
+		else if (el.href.match(/\/assets\//) && !el.href.match(/\.(jpe?g|bmp|png|gif)$/i)) {
 			_gaq.push(["_trackEvent","Downloads",el.href.match(/\/assets\/(.*)/)[1],ref]);
-			if ( !el.target ) {
+			if (!el.target){
 				setTimeout(function(){document.location.href=el.href;}.bind(el),500);
 				return false;
 			}
 		}
 	}
 }
-/* Add tracker to all document click events */
-var b = document.body;
-var o = b.onclick;
-if ( typeof o != "function" ) b.onclick = gaLt;
-else {
-	b.onclick = function() {
-		o();
-		gaLt();
-	}
-}
+var d=document;
+if(d.addEventListener)d.addEventListener("click",_gaLt);
+else if(d.attachEvent)d.attachEvent("onclick",_gaLt);
